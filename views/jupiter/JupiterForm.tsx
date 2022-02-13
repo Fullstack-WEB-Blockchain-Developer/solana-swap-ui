f006939b0888fff67e2eefcdd76a6014a05010cb
Jeffrey Yeung <jeffyyw@gmail.com>
2022-02-14 06:11:21 +0800

    select token wip

--- views/jupiter/JupiterForm.tsx
+++ views/jupiter/JupiterForm.tsx
@@ -8,12 +8,14 @@ import FeeInfo from "./FeeInfo";
 import { getSPLTokenData, ISplToken } from "../../utils/web3";
 import SplTokenList from "../commons/SplTokenList";
 import style from "../../styles/jupiter.module.sass";
+import { ArrowDownIcon, CloseIcon } from "@chakra-ui/icons";
 
 const CHAIN_ID = ENVChainId.MainnetBeta;
 interface IJupiterFormProps {}
 interface IToken {
   mint: string;
   symbol: string;
+  logoURI: string;
 }
 
 type UseJupiterProps = Parameters<typeof useJupiter>[0];
@@ -86,9 +88,11 @@ const JupiterForm: FunctionComponent<IJupiterFormProps> = (props) => {
       let tokenInfo: IToken = {
         mint: "",
         symbol: "",
+        logoURI: "",
       };
       tokenInfo["mint"] = t;
       tokenInfo["symbol"] = tokenMap.get(t)?.name || "unknown";
+      tokenInfo["logoURI"] = tokenMap.get(t)?.logoURI || "";
       return tokenInfo;
     });
   };
@@ -131,89 +135,161 @@ const JupiterForm: FunctionComponent<IJupiterFormProps> = (props) => {
     return () => {};
   }, [wallet.connected]);
 
+  const [showTokenSelect, setShowTokenSelect] = useState<Boolean>(false);
+  const [showOutTokenSelect, setShowOutTokenSelect] = useState<Boolean>(false);
+
   return (
-    <div style={{ display: "flex" }}>
-      <div>
-        <SplTokenList splTokenData={splTokenData} />
-      </div>
+    <div className={style.jupiterPage}>
+      <div>{/* <SplTokenList splTokenData={splTokenData} /> */}</div>
       <div className={style.jupiterFormModal}>
-        {/* <div className={style.title}>Jupiter</div> */}
-        <div className={style.selectBlock}>
-          <label htmlFor="inputMint">From</label>
-          <select
-            className={style.select}
-            id="inputMint"
-            name="inputMint"
-            value={formValue.inputMint?.toBase58()}
-            onChange={(e) => {
-              console.log(e.currentTarget.value);
-              console.log(formValue);
-              const pbKey = new PublicKey(e.currentTarget.value);
-              if (pbKey) {
+        <div className={style.tokenSelect}>
+          <div>From</div>
+          <div className={style.inputBlock}>
+            <input
+              className={style.searchTokenInput}
+              name="amount"
+              id="amount"
+              value={formValue.amount}
+              type="text"
+              pattern="[0-9]*"
+              onInput={(e: any) => {
+                let newValue = Number(e.target?.value || 0);
+                newValue = Number.isNaN(newValue) ? 0 : newValue;
                 setFormValue((val) => ({
                   ...val,
-                  inputMint: pbKey,
+                  amount: Math.max(newValue, 0),
                 }));
-              }
-            }}
-          >
-            {inputList.map((t: IToken) => {
-              return (
-                <option key={t.mint} value={t.mint}>
-                  {t.symbol}
-                </option>
-              );
-            })}
-          </select>
+              }}
+            />
+            <button onClick={() => setShowTokenSelect(!showTokenSelect)}>
+              <span>{inputTokenInfo?.symbol && inputTokenInfo?.symbol}</span>
+              <ArrowDownIcon w={20} h={20} />
+            </button>
+          </div>
+
+          {showTokenSelect && (
+            <div
+              className={style.overlay}
+              onClick={() => setShowTokenSelect(!showTokenSelect)}
+            >
+              <div
+                className={style.tokenContainer}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className={style.header}>
+                  <div>Select a token</div>
+                  <div
+                    className={style.closeIcon}
+                    onClick={() => setShowTokenSelect(!showTokenSelect)}
+                  >
+                    <CloseIcon w={20} h={20} />
+                  </div>
+                </div>
+                <div className={style.tokenListTitleRow}>
+                  <div>Token name</div>
+                </div>
+                <div className={style.list}>
+                  {inputList.map((t: IToken) => {
+                    return (
+                      <div
+                        key={t.mint}
+                        data-mint={t.mint}
+                        className={style.tokenRow}
+                        onClick={(e) => {
+                          const dataMint =
+                            e.currentTarget.getAttribute("data-mint") || "";
+                          const pbKey = new PublicKey(dataMint);
+                          if (pbKey) {
+                            setFormValue((val) => ({
+                              ...val,
+                              inputMint: pbKey,
+                            }));
+                          }
+                          setShowTokenSelect(!showTokenSelect);
+                        }}
+                      >
+                        <div className={style.tokenItem}>
+                          <img
+                            src={t.logoURI}
+                            alt=""
+                            className={style.tokenLogo}
+                          />
+                          <div>{t.symbol}</div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
 
-        <div className={style.inputBlock}>
-          <label htmlFor="amount">
-            Input Amount {inputTokenInfo?.symbol && inputTokenInfo?.symbol}
-          </label>
-          <input
-            className={style.searchTokenInput}
-            name="amount"
-            id="amount"
-            value={formValue.amount}
-            type="text"
-            pattern="[0-9]*"
-            onInput={(e: any) => {
-              let newValue = Number(e.target?.value || 0);
-              newValue = Number.isNaN(newValue) ? 0 : newValue;
-              setFormValue((val) => ({
-                ...val,
-                amount: Math.max(newValue, 0),
-              }));
-            }}
-          />
-        </div>
+        <div className={style.jupiterFormModal}>
+          <div className={style.tokenSelect}>
+            <div>To</div>
+            <button onClick={() => setShowOutTokenSelect(!showOutTokenSelect)}>
+              <span>{outputTokenInfo?.symbol && outputTokenInfo?.symbol}</span>
+              <ArrowDownIcon w={20} h={20} />
+            </button>
+          </div>
 
-        <div className={style.selectBlock}>
-          <label htmlFor="outputMint">To</label>
-          <select
-            className={style.select}
-            id="outputMint"
-            name="outputMint"
-            value={formValue.outputMint?.toBase58()}
-            onChange={(e) => {
-              const pbKey = new PublicKey(e.currentTarget.value);
-              if (pbKey) {
-                setFormValue((val) => ({
-                  ...val,
-                  outputMint: pbKey,
-                }));
-              }
-            }}
-          >
-            {outputList.map((t: IToken) => {
-              return (
-                <option key={t.mint} value={t.mint}>
-                  {t.symbol}
-                </option>
-              );
-            })}
-          </select>
+          {showOutTokenSelect && (
+            <div
+              className={style.overlay}
+              onClick={() => setShowOutTokenSelect(!showOutTokenSelect)}
+            >
+              <div
+                className={style.tokenContainer}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className={style.header}>
+                  <div>Select a token</div>
+                  <div
+                    className={style.closeIcon}
+                    onClick={() => setShowOutTokenSelect(!showOutTokenSelect)}
+                  >
+                    <CloseIcon w={20} h={20} />
+                  </div>
+                </div>
+                <div className={style.tokenListTitleRow}>
+                  <div>Token name</div>
+                </div>
+                <div className={style.list}>
+                  {outputList.map((t: IToken) => {
+                    return (
+                      <div
+                        key={t.mint}
+                        data-mint={t.mint}
+                        className={style.tokenRow}
+                        onClick={(e) => {
+                          const dataMint =
+                            e.currentTarget.getAttribute("data-mint") || "";
+                          const pbKey = new PublicKey(dataMint);
+                          if (pbKey) {
+                            setFormValue((val) => ({
+                              ...val,
+                              outputMint: pbKey,
+                            }));
+                          }
+                          setShowOutTokenSelect(!showOutTokenSelect);
+                        }}
+                      >
+                        <div className={style.tokenItem}>
+                          <img
+                            src={t.logoURI}
+                            alt=""
+                            className={style.tokenLogo}
+                          />
+                          <div>{t.symbol}</div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
 
         {routes && (
